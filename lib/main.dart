9a59d913dc77034fe51c0072c70a7573bf287125
Jessi-92 <pasanteti2@happy.ec>
2026-04-01 15:16:44 -0500

    Primer commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material App',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Ejemplo Inputs'),
+        ),
+      ),
+    );
+  }
+}
